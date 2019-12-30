a4ac38f02d8101b5f788cd4d39504de86f32c0cf
Shipra <drshipradyundi@gmail.com>
2019-12-30 10:27:15 -0800

    adding json data

---
+++ app/data/friends.js
@@ -0,0 +1,25 @@
+var friends = [{
+        name: "Rahul",
+        photo: "",
+        scores: [5, 1, 4, 4, 5, 5, 1, 3, 4, 4]
+    },
+    {
+        name: "Sara",
+        photo: "",
+        scores: [4, 2, 3, 3, 5, 5, 2, 3, 4, 5]
+    }, {
+        name: "Nicole",
+        photo: "",
+        scores: [5, 2, 3, 3, 5, 4, 2, 3, 4, 5]
+    }, {
+        name: "Shadow",
+        photo: "",
+        scores: [3, 1, 5, 5, 3, 2, 4, 3, 4, 5]
+    }, {
+        name: "Aanya",
+        photo: "",
+        scores: [4, 5, 3, 1, 4, 5, 2, 4, 5, 5]
+    }
+];
+
+module.exports = friends;
