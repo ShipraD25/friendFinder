c006aeab01ef21549f53911d085ca0b1d8bed525
Shipra <drshipradyundi@gmail.com>
2019-12-30 20:26:09 -0800

    adding images to data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,23 +1,23 @@
 var friends = [{
         name: "Rahul",
-        photo: "",
+        photo: "https://picsum.photos/id/1020/367/267",
         scores: [5, 1, 4, 4, 5, 5, 1, 3, 4, 4]
     },
     {
         name: "Sara",
-        photo: "",
+        photo: "https://picsum.photos/id/1006/367/267",
         scores: [4, 2, 3, 3, 5, 5, 2, 3, 4, 5]
     }, {
         name: "Nicole",
-        photo: "https://i.picsum.photos/id/1/5616/3744.jpg",
+        photo: "https://picsum.photos/id/237/367/267",
         scores: [5, 2, 3, 3, 5, 4, 2, 3, 4, 5]
     }, {
         name: "Shadow",
-        photo: "",
+        photo: "https://picsum.photos/id/169/367/267",
         scores: [3, 1, 5, 5, 3, 2, 4, 3, 4, 5]
     }, {
         name: "Aanya",
-        photo: "",
+        photo: "https://picsum.photos/id/177/367/267",
         scores: [4, 5, 3, 1, 4, 5, 2, 4, 5, 5]
     }
 ];
